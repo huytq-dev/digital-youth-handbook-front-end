3909b3307cf335013fe1fcdeae8ba7f47c6786c1
Huy Quang <hyperrhy1412@gmail.com>
2025-12-01 19:45:47 +0700

    update: config BE api

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // export const host = 'https://tuoi-tre-online-backend.onrender.com';
-export const host = 'http://tuoitreonline.runasp.net';
+export const host = 'https://tuoitreonline.runasp.net';
 
 export const api_version = 'api';
 export const baseUrl = `${host}/${api_version}/`;
